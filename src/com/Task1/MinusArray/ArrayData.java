9f2bcd1bba8d2f8d2042d9e94c21cd0c931cea72
VincentVegaV <forgitmail@gmail.com>
2018-07-24 00:02:08 +0300

    0.01

--- src/com/Task1/MinusArray/ArrayData.java
+++ src/com/Task1/MinusArray/ArrayData.java
@@ -1,4 +1,8 @@
 package com.Task1.MinusArray;
 
 public class ArrayData {
+    String[] array1 = {"Alex", "Dima", "Kate", "Galina", "Ivan"};
+    String[] array2 = {"Dima", "Ivan", "Kate"};
+
+
 }
--- src/com/Task1/MinusArray/ArraySubtraction.java
+++ src/com/Task1/MinusArray/ArraySubtraction.java
@@ -1,4 +1,21 @@
 package com.Task1.MinusArray;
 
 public class ArraySubtraction {
+
+    public String[] getSubtractedArray(String[] array1, String[] array2)
+    {
+        int n = array1.length-array2.length;
+        String[] resultArray = new String[n];
+
+        for(int i=0; i < array2.length; i++)
+        {
+            for(int j=0; j < array1.length; j++)
+            {
+
+            }
+        }
+
+        return resultArray;
+    }
+
 }
