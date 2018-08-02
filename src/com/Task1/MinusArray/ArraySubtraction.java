9616d5358c6edcd6f784e719654c370c29ea130f
VincentVegaV <forgitmail@gmail.com>
2018-08-02 20:53:06 +0300

    0.02
    Notes:
    checkTypeArray method added

--- src/com/Task1/MinusArray/ArraySubtraction.java
+++ src/com/Task1/MinusArray/ArraySubtraction.java
@@ -1,15 +1,20 @@
 package com.Task1.MinusArray;
 
-public class ArraySubtraction {
+public class ArraySubtraction
+{
 
-    public String[] getSubtractedArray(String[] array1, String[] array2) {
+    public String[] getSubtractedArray(String[] array1, String[] array2)
+    {
 
         int n = array1.length - array2.length;
         String[] resultArray = new String[n];
 
-        for (int i = 0; i < array2.length; i++) {
-            for (int j = 0; j < array1.length; j++) {
-                if (array1[j].equals(array2[i])) {
+        for (int i = 0; i <= array2.length - 1; i++)
+        {
+            for (int j = 0; j <= array1.length - 1; j++)
+            {
+                if (array1[j].equals(array2[i]))
+                {
 
                 }
             }
@@ -25,5 +30,10 @@ public class ArraySubtraction {
 
         return type;
     }
+
+
+
+//метод проверки на пустой массив
+
 
 }
