03f7d258f09ade9b661c1bcb7a862c99dfd1d109
VincentVegaV <forgitmail@gmail.com>
2018-08-05 17:55:20 +0300

    0.07
    ArrayList subtraction fixed

--- src/com/Task1/MinusArray/ArrayData.java
+++ src/com/Task1/MinusArray/ArrayData.java
@@ -1,12 +1,7 @@
 package com.Task1.MinusArray;
 
-import java.util.ArrayList;
-
 class ArrayData {
     String[] array1 = {"Alex", "Dima", "Kate", "Galina", "Ivan"};
     String[] array2 = {"Dima", "Ivan", "Kate"};
-
-    ArrayList<String> arrayL1 = new ArrayList<>();
-    ArrayList<String> arrayL2 = new ArrayList<>();
 
 }
